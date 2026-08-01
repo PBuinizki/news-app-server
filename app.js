9c76ff0c14e0c4d8f5871fa6936fb1706dbd735f
PBuinizki <pbuinizki@gmail.com>
2026-08-01 15:05:24 +0400

    feat(fs): добавил multer

--- app.js
+++ app.js
@@ -17,10 +17,29 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
-mongoose
+/* mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log("Подключился к Atlas!"))
-    .catch((err) => console.error('Не получилось подключиться к Atlas: ', err));
+    .catch((err) => console.error('Не получилось подключиться к Atlas: ', err)); */
+
+if(!fs.existsSync('uploads')) {
+    fs.mkdirSync('uploads');
+}
+
+app.use('/uploads', express.static('uploads'));
+
+const stroge = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, 'uploads/'),
+    filename: (req, file, cb) => {
+        const unique = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, unique + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({
+    storage,
+    limits: {fieldSize: 10 * 1024 * 1024}
+});
 
 server.listen(PORT, () => {
     console.log(`Сервер запустился: http://localhost:${PORT}`);
