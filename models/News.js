6d5eac0c8f6262ee5a9684a03a51986787455ac4
PBuinizki <pbuinizki@gmail.com>
2026-08-02 11:24:50 +0400

    feat(database): добавил модели для пользователей и для новостей

--- models/News.js
+++ models/News.js
@@ -1,1 +1,62 @@
+/** 
+ * @fileoverview Модель новости
+ * @requires mongoose
+*/
 
+const mongoose = require('mongoose');
+
+/** 
+ * Схема новости
+ * @typedef { Object } News
+ * @property { string } title - Заголовок новости
+ * @property { string } content - HTML-содеримое статьи
+ * @property { string[] } images - Массив URL изображений
+ * @property { string[] } files - Массив URL прикрепленных файлов
+ * @property { string[] } quotes - Массив цитат
+ * @property { string } status - Статус новости: "черновик" или "опубликован"
+ * @property { ObjectId } authorId - Ссылка на пользователя-автора
+ * @property { Date } createdAt - Дата создания
+ * @property { Date } updatedAt - Дата обновления
+*/
+
+const newsSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Заголовок не может быть пустым!'],
+            trim: true,
+            maxlength: [200, 'Не больше 200 символов']
+        },
+        content: {
+            type: String,
+            required: [true, 'Не может быть пустой новости']
+        },
+        images: {
+            type: [String],
+            default: []
+        },
+        files: {
+            type: [String],
+            default: []
+        },
+        quotes: {
+            type: [String],
+            default: []
+        },
+        status: {
+            type: String,
+            enum: ['draft', 'published'],
+            default: 'draft'
+        },
+        authorId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+newsSchema.index({ status: 1, publishAt: 1 });
+
+module.exports = mongoose.model('News', newsSchema)
--- models/User.js
+++ models/User.js
@@ -1,1 +1,35 @@
+/**
+ *  @fileoverview Модель пользователей
+ *  @requires mongoose
+ */
 
+const mongoose = require('mongoose');
+
+/** 
+ * Схема пользователя
+ * @typedef { Object } User
+ * @property { string } email - Уникальная электроная почта пользователя
+ * @property { string } passwordHash - Хешированный пароль
+ * @property { Date } createdAt - Дата создания
+ * @property { Date } updatedAt - Дата обновления
+*/
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            require: [true, 'Требуется email'],
+            unique: true,
+            trim: true,
+            lowercase: true,
+            match: [/^\S+@\S+\.\S+$/, 'Некорректный email']
+        },
+        passwordHash: {
+            type: String,
+            require: [true, 'Требуется пароль']
+        }
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('User', userSchema);
